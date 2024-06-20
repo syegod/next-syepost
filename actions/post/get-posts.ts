008015647576085a2c31adff6292efb2492dc182
syegod <ovisochin362@gmail.com>
2024-06-20 22:06:46 +0200

    get-posts fix

--- actions/post/get-posts.ts
+++ actions/post/get-posts.ts
@@ -6,31 +6,29 @@ import { revalidatePath } from "next/cache";
 export const get_posts = async (orderBy: 'popular' | 'latest' = 'latest') => {
     try {
         let posts
-        switch (orderBy) {
-            case 'popular': {
-                posts = db.post.findMany({
-                    include: {
-                        author: true,
-                        likes: true,
-                        comments: true
-                    },
-                    orderBy: {
-                        views: 'desc'
-                    }
-                });
-            }
-            case 'latest': {
-                posts = db.post.findMany({
-                    include: {
-                        author: true,
-                        likes: true,
-                        comments: true
-                    },
-                    orderBy: {
-                        createdAt: 'desc'
-                    }
-                });
-            }
+        if (orderBy === 'latest') {
+            posts = db.post.findMany({
+                include: {
+                    author: true,
+                    likes: true,
+                    comments: true
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                }
+            });
+        }
+        if (orderBy === 'popular') {
+            posts = db.post.findMany({
+                include: {
+                    author: true,
+                    likes: true,
+                    comments: true
+                },
+                orderBy: {
+                    views: 'desc'
+                }
+            });
         }
 
         revalidatePath('/')
