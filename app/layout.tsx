c62e6305a3bb6c8e0d7dea7435ac30b675161ee2
syegod <ovisochin362@gmail.com>
2024-06-21 20:02:46 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout(
   
   return (
     <html lang="en">
-      <body className={`${inter.className} relative scroll pb-10 overflow-x-hidden dark`}>
+      <body className={`${inter.className} relative scroll pb-10 overflow-x-hidden dark a`}>
         <Header />
         {props.children}
         <NextTopLoader />
