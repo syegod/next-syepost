c41ea844d939e3f242e2ee1877faf4fa083e388c
syegod <ovisochin362@gmail.com>
2024-06-21 18:42:50 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-        appDir: true, // Ensure appDir is enabled for App Router
-    },
     output: 'standalone', // Required for serverless deployment
     images: {
         remotePatterns: [
